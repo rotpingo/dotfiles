470da564ad17b68c15d868e966397cb15309303e
rotpingo <viorel.moschoi@gmail.com>
2025-10-31 17:41:42 +0100

    addes some params to toggleterm plugin

--- nvim/lua/plugins/toggleterm.lua
+++ nvim/lua/plugins/toggleterm.lua
@@ -1,6 +1,19 @@
 return {
 	{
 		-- amongst your other plugins
-		{ "akinsho/toggleterm.nvim", version = "*", config = true },
+		{
+			"akinsho/toggleterm.nvim",
+			version = "*",
+			config = true,
+			opts = {
+				size = 15,
+				open_mapping = [[<leader>tt]],
+				shade_terminals = true,
+				direction = "horizontal",
+				float_opts = {
+					border = "curved",
+				},
+			},
+		},
 	},
 }
